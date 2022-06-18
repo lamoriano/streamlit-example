f1dfaac207e52bfcf77850c2f38bc59eea7523de
lamoriano <lamoriano@gmail.com>
2022-06-18 19:10:35 +0700

    dummy streamlit version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,127 @@
-from collections import namedtuple
-import altair as alt
-import math
+#from collections import namedtuple
+#import altair as alt
+#import math
 import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.set_page_config(
+    page_title="Fun Fact",
+    page_icon=":bar_chart:",
+    layout="wide"
+)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+df = pd.read_excel(
+    io = 'seasia_articles_funfact.xlsx',
+    engine = 'openpyxl',
+    sheet_name= 'DATA'
+)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+# ----- SIDEBAR -------
+st.sidebar.header("Filter: ")
+year = st.sidebar.selectbox(
+    "Select the Year: ",
+    options=df["Year"].unique(),
+    #default=df["Year"].unique()
+)
+df_year = df.query("Year == @year")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+country = st.sidebar.multiselect(
+    "Select the Country: ",
+    options=df_year["Country"].unique(),
+    default=df_year["Country"].unique()
+)
+month = st.sidebar.multiselect(
+    "Select the Month: ",
+    options=df_year["Month"].unique(),
+    default=df_year["Month"].unique()
+)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+df_selection = df_year.query(
+    "Country == @country & Month == @month"
+)
+
+#st.dataframe(df_selection)
+
+# ---- MAINPAGE -----
+st.title(f":bar_chart: Summary of {year}")
+st.markdown("##")
+
+# --- KPI ---
+total_share = int(df_selection["Share"].sum())
+total_article = int(df_selection["Title"].count())
+total_author = len(pd.unique(df_selection["Author"]))#int(df_selection["Author"].unique().count())
+
+left_col, mid_col, right_col = st.columns(3)
+with left_col:
+    st.subheader(":star: Total Shared: ")
+    st.subheader(f"{total_share} shared")
+with mid_col:
+    st.subheader(":newspaper: Total Article: ")
+    st.subheader(f"{total_article} article(s)")
+with right_col:
+    st.subheader(":bust_in_silhouette: Total Author: ")
+    st.subheader(f"{total_author} author(s)")
+
+st.markdown("---")
+
+# Share by Author
+share_author = (
+        df_selection.groupby(by=["Author"]).sum()[["Share"]].sort_values(by="Share")
+    )
+
+fig_author = px.bar(
+    share_author,
+    x="Share",
+    y=share_author.index,
+    orientation="h",
+    title="<b>Shared by Author</b>",
+    color_discrete_sequence=["#0083B8"] * len(share_author),
+    template="plotly_white",
+)
+
+fig_author.update_layout(
+    plot_bgcolor = "rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+
+
+
+# Share by React
+share_react = (
+    df_selection.groupby(by=["React_1"]).sum()[["Share"]].sort_values(by="Share")
+)
+fig_react = px.bar(
+    share_react,
+    x=share_react.index,
+    y="Share",
+    orientation="v",
+    title="<b>Shared by React</b>",
+    color_discrete_sequence=["#0083B8"] * len(share_react),
+    template="plotly_white",
+)
+
+fig_react.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+
+left_col, right_col = st.columns(2)
+left_col.plotly_chart(fig_author, use_container_width=True)
+right_col.plotly_chart(fig_react, use_container_width=True)
+
+# Hide Style
+hide_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_style, unsafe_allow_html=True)
